fe4a11504a72ffd08b0d27e000d81712a25165df
Zeid Magboub <zeid.magboub@litc.ly>
2020-12-12 16:33:24 +0200

    fist and prolly last

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import image from '../images/monstera-leaf.jpg';
+
+const Home = () => {
+  return (
+    <main>
+      <img
+        src={image}
+        alt="Monstera leaf"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Hello, I'm Zeid.
+        </h1>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
